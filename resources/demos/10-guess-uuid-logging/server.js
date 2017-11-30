ea617db0d11eaf261b68489faf1d892d84cb0643
Stephen Lau <stephen@edument.se>
2017-11-30 15:38:04 +0100

    Initial commit

---
+++ resources/demos/10-guess-uuid-logging/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+
+import uuid from 'uuid/v4';
+
+//SHOW_IN_SLIDE_START_1
+let uuidToGuess;
+function resetUuid() {
+  uuidToGuess = uuid();
+  console.log(`uuid to guess: ${uuidToGuess}`);
+}
+//SHOW_IN_SLIDE_END_1
+resetUuid();
+
+
+let app = express();
+app.get('/', (req, res) => {
+  res.status(200).send('<h1>Guess a uuid!</h1><br>' +
+    'Guess here: /guess/:uuid-to-guess<br>' +
+    'Reset Game Here: /reset');
+});
+
+app.get('/guess/:uuid', (req, res) => {
+  res.status(200).json({'correct': uuidToGuess === req.params.uuid});
+});
+
+app.get('/reset', (req, res) => {
+  resetUuid();
+  res.status(200).send('uuid to guess is now reset!');
+});
+
+// Spin the server up on localhost:8080
+app.listen(8080, () => console.log('Server running on port 8080'));
